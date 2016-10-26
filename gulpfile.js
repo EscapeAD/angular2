2120e57f8216fcdbd15a1fdf8705cea07d922348
Adam Tse <mr.adam.tse@gmail.com>
2016-10-26 04:12:58 -0400

    add presets

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-var gulp = require('gulp');
+var gulp  = require('gulp'),
+    babel = require('gulp-babel');
 
 gulp.task('default', function() {
   // place code for your default task here
+  return gulp.src("src/app.js")
+             .pipe(babel())
+             .pipe(gulp.dest("dist"));
 });
---
+++ src/app.js
@@ -0,0 +1,1 @@
+const adam = test;
