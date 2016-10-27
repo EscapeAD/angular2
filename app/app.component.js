4620294bc7f55edeab0e180af69a815831438b63
Adam Tse <mr.adam.tse@gmail.com>
2016-10-27 03:17:21 -0400

    template

--- app/app.component.js
+++ app/app.component.js
@@ -2,7 +2,7 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>This is my first angular app</h1>'
+      template: '<h1>Welcome to the zone</h1><p>What is the plan?</p>'
     })
     .Class({
       constructor: function() {}
