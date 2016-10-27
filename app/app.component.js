3badbed148bb1d20a5095305d06d4e06fd24cb42
Adam Tse <mr.adam.tse@gmail.com>
2016-10-27 04:25:25 -0400

    changes to button event

--- app/app.component.js
+++ app/app.component.js
@@ -2,16 +2,18 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Welcome to the zone {{name}}</h1><p>What is the plan? <button (click)="buttonTest()"> Conole.log</button></p>'
+      template: '<h1>Welcome to the zone {{name}}</h1><p>What is the plan? <button (click)="buttonTest()"> {{check}}</button></p>'
     })
     .Class({
       constructor: function() {
-        this.name = 'Newbie';
+        this.name      = 'Newbie';
+        this.check     = 'button';
       },
       buttonTest(){
         console.log('Testing round 2');
         if (this.name == 'Number2'){
           this.name = 'OKAY NEWBIE';
+          this.check = 'THIS IS NOT A BUTTON'
         } else {
           this.name = 'Number2';
         }
