8d03e8df697f1ab419e4692743b60242af90f8dd
Adam Tse <mr.adam.tse@gmail.com>
2016-10-27 03:46:26 -0400

    event set for button

--- app/app.component.js
+++ app/app.component.js
@@ -2,11 +2,19 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Welcome to the zone {{name}}</h1><p>What is the plan?</p>'
+      template: '<h1>Welcome to the zone {{name}}</h1><p>What is the plan? <button (click)="buttonTest()"> Conole.log</button></p>'
     })
     .Class({
       constructor: function() {
         this.name = 'Newbie';
+      },
+      buttonTest(){
+        console.log('Testing round 2');
+        if (this.name == 'Number2'){
+          this.name = 'OKAY NEWBIE';
+        } else {
+          this.name = 'Number2';
+        }
       }
     });
 })(window.app || (window.app = {}));
