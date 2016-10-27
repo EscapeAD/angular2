99a36c3cd0d81041c88831f08d5395579f6eb1de
Adam Tse <mr.adam.tse@gmail.com>
2016-10-27 03:33:18 -0400

    render export name

--- app/app.component.js
+++ app/app.component.js
@@ -2,9 +2,11 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>Welcome to the zone</h1><p>What is the plan?</p>'
+      template: '<h1>Welcome to the zone {{name}}</h1><p>What is the plan?</p>'
     })
     .Class({
-      constructor: function() {}
+      constructor: function() {
+        this.name = 'Newbie';
+      }
     });
 })(window.app || (window.app = {}));
